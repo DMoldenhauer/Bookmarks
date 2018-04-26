75abc4a11449f78d169e5c0b597a603b95f0283a
Laurie Aaronson <aaronson.laurie@gmail.com>
2018-04-26 15:33:11 -0500

    updated if else for user with JawsDB info

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ var db        = {};
 // var mysql     = require("mysql");
 // var connection
 
-if (process.env.JAWSDB_URL) {
+// if (process.env.JAWSDB_URL) {
 //   connection = mysql.createConnection(process.env.JAWSDB_URL);
 // } else {
 //   connection = mysql.createConnection ({
--- models/users.js
+++ models/users.js
@@ -3,14 +3,25 @@ var Sequelize = require("sequelize");
 // sequelize (lowercase) references my connection to 
 // var sequelize = require("../config/config.json"); 
 
-var connection = new Sequelize({
+if (process.env.JAWSDB_URL) {
+  var connection = new Sequelize({
+    username: "hura4fj4dqe072x4",
+    password: "hg8bhez2fmlj5eoj",
+    database: "ufyfu8ke0nm851sb",
+    port: 3306,
+    host: "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    dialect: "mysql"
+  })
+  } else {
+  var connection = new Sequelize({
     username: "root",
     password: "root",
     database: "bootcamp_bookmarksDB",
     port: 8889,
-    host: "127.0.0.1",
+    host: "localhost",
     dialect: "mysql"
-})
+  })
+  }
 
 
 //creates table user (and sequelize will add the "s" to the end)
